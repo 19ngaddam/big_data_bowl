9870afb26e081abc0151267bd39d8af63fa41f14
Nikhil Gaddam <nikgad123@gmail.com>
2019-11-21 20:40:50 -0500

    initial data and kaggle stuff

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# big_data_bowl
+#NFL big data bowl 2019-2020
+
+#How many yards is a run expected to gain?
+
---
+++ data_extraction/data.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Data extraction from train.csv
+
+Created on Thu Nov 21 20:38:27 2019
+
+@author: nikhil
+"""
+
+print("Hello NFL")
+
