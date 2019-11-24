78daaef11faf87eda2c062e8d528b81d4db9009e
Nikhil Gaddam <nikgad123@gmail.com>
2019-11-23 22:23:01 -0500

    changed data extraction to use pandas library

--- data_extraction/data.py
+++ data_extraction/data.py
@@ -11,24 +11,26 @@ import pandas
 print("Hello NFL")
 
 #colnames = ['GameId','PlayId','Team','X','Y','S','A','Dis','Orientation','Dir','NflId','DisplayName','JerseyNumber','Season','YardLine','Quarter','GameClock','PossessionTeam','Down','Distance','FieldPosition','HomeScoreBeforePlay','VisitorScoreBeforePlay','NflIdRusher','OffenseFormation','OffensePersonnel','DefendersInTheBox','DefensePersonnel','PlayDirection','TimeHandoff','TimeSnap','Yards','PlayerHeight','PlayerWeight','PlayerBirthDate','PlayerCollegeName','Position','HomeTeamAbbr','VisitorTeamAbbr','Week','Stadium','Location','StadiumType','Turf','GameWeather','Temperature','Humidity','WindSpeed','WindDirection']
-#data = pandas.read_csv('C:\Users\nikhil\Documents\GitHub\big_data_bowl\resources\train.csv', names=colnames)
+train_df = pandas.read_csv('C:\\Users\\nikhil\\Documents\\GitHub\\big_data_bowl\\resources\\train.csv', low_memory=False)
 
-
-# open the file in universal line ending mode 
-with open('C:\\Users\\nikhil\\Documents\\GitHub\\big_data_bowl\\resources\\train.csv','rU') as infile:
-  # read the file as a dictionary for each row ({header : value})
-  reader = csv.DictReader(infile)
-  data = {}
-  for row in reader:
-    for header, value in row.items():
-      try:
-        data[header].append(value)
-      except KeyError:
-        data[header] = [value]
-
-# extract the variables you want
-gameid = data['GameId']
-print("First game: "+ gameid[0])
-#print(gameid[0])
+print(train_df)
+# =============================================================================
+# # open the file in universal line ending mode 
+# with open('C:\\Users\\nikhil\\Documents\\GitHub\\big_data_bowl\\resources\\train.csv','rU') as infile:
+#   # read the file as a dictionary for each row ({header : value})
+#   reader = csv.DictReader(infile)
+#   data = {}
+#   for row in reader:
+#     for header, value in row.items():
+#       try:
+#         data[header].append(value)
+#       except KeyError:
+#         data[header] = [value]
+# 
+# # extract the variables you want
+# gameid = data['GameId']
+# print("First game: "+ gameid[0])
+# #print(gameid[0])
+# =============================================================================
 
 
