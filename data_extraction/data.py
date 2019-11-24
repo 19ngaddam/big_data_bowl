ea1a86509d14891ddee9497293c40d4b6a0511c3
Nikhil Gaddam <nikgad123@gmail.com>
2019-11-23 23:13:42 -0500

    fixed common file path naming

--- data_extraction/data.py
+++ data_extraction/data.py
@@ -8,11 +8,11 @@ Created on Thu Nov 21 20:38:27 2019
 """
 import csv
 import pandas
+import os
 print("Hello NFL")
-
 #colnames = ['GameId','PlayId','Team','X','Y','S','A','Dis','Orientation','Dir','NflId','DisplayName','JerseyNumber','Season','YardLine','Quarter','GameClock','PossessionTeam','Down','Distance','FieldPosition','HomeScoreBeforePlay','VisitorScoreBeforePlay','NflIdRusher','OffenseFormation','OffensePersonnel','DefendersInTheBox','DefensePersonnel','PlayDirection','TimeHandoff','TimeSnap','Yards','PlayerHeight','PlayerWeight','PlayerBirthDate','PlayerCollegeName','Position','HomeTeamAbbr','VisitorTeamAbbr','Week','Stadium','Location','StadiumType','Turf','GameWeather','Temperature','Humidity','WindSpeed','WindDirection']
-train_df = pandas.read_csv('C:\\Users\\nikhil\\Documents\\GitHub\\big_data_bowl\\resources\\train.csv', low_memory=False)
-
+train_df = pandas.read_csv(os.path.abspath('resources/train.csv'), low_memory=False)
+                          
 print(train_df)
 # =============================================================================
 # # open the file in universal line ending mode 
