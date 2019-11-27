cbc4d172f1bb50b3fea78f595abd0bcf4a1dca3c
Nikhil Gaddam <nikgad123@gmail.com>
2019-11-27 17:37:11 -0500

    added kaggle notebook

--- data_extraction/data.py
+++ data_extraction/data.py
@@ -9,11 +9,24 @@ Created on Thu Nov 21 20:38:27 2019
 import csv
 import pandas
 import os
+from kaggle.competitions import nflrush 
+
+
+import numpy
+import sklearn
+import lightgbm
 print("Hello NFL")
 #colnames = ['GameId','PlayId','Team','X','Y','S','A','Dis','Orientation','Dir','NflId','DisplayName','JerseyNumber','Season','YardLine','Quarter','GameClock','PossessionTeam','Down','Distance','FieldPosition','HomeScoreBeforePlay','VisitorScoreBeforePlay','NflIdRusher','OffenseFormation','OffensePersonnel','DefendersInTheBox','DefensePersonnel','PlayDirection','TimeHandoff','TimeSnap','Yards','PlayerHeight','PlayerWeight','PlayerBirthDate','PlayerCollegeName','Position','HomeTeamAbbr','VisitorTeamAbbr','Week','Stadium','Location','StadiumType','Turf','GameWeather','Temperature','Humidity','WindSpeed','WindDirection']
-train_df = pandas.read_csv(os.path.abspath('resources/train.csv'), low_memory=False)
-                          
-print(train_df)
+train_df = pandas.read_csv(os.path.abspath('resources/train.csv'), low_memory=False)                    
+
+#env=nflrush.make_env()=['GameId','PlayId',]]
+
+
+
+
+
+
+
 # =============================================================================
 # # open the file in universal line ending mode 
 # with open('C:\\Users\\nikhil\\Documents\\GitHub\\big_data_bowl\\resources\\train.csv','rU') as infile:
